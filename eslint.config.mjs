e93e1b9a25bfaa1063e46eabc6e8bfb9bc074407
seminiariyawansha <ariyawa-im23029@stu.kln.ac.lk>
2026-09-05 10:42:14 +0530

    fix: add jest globals to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,14 @@
-import js from "@eslint/js";
-import globals from "globals";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js';
+import globals from 'globals';
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-]);
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest
+      }
+    }
+  }
+];
